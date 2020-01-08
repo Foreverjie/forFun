fabc8d28176d7cb50e0bdccb38725f861f1b653e
Foreverjie <864129545@qq.com>
2020-01-08 22:36:59 +0800

    update

--- my-react-app/src/components/TodoItem.js
+++ my-react-app/src/components/TodoItem.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 class TodoItem extends Component {
 
   render() {
     // console.log(this.props.todos)
-    return <h3 key={this.props.todo.id}>{ this.propstodo.title }</h3>
+    return <h3 key={this.props.todo.id}>{ this.props.todo.title }</h3>
   }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
 }
 
 export default TodoItem
